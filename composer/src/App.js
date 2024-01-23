3f24c0ab330193d9714028679a79d48996c43d92
abymosa <dre7am@hotmail.com>
2024-01-23 12:21:30 +0000

    composer workflow

--- composer/src/App.js
+++ composer/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div>
       <h1> app component </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus cum, nam cupiditate cumque voluptas natus assumenda sunt delectus quam aperiam earum obcaecati at explicabo corrupti ex accusamus. Cum, expedita inventore.</p>
       <hr />
 
       <Users />
